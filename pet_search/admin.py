77b2c2f033a7944d9707f1d827580f6ab7b8f9f1
Marc-Andre Boily <mandre.boily@gmail.com>
2016-10-29 19:48:48 -0400

    Add basic pet model

--- pet_search/admin.py
+++ pet_search/admin.py
@@ -1,3 +1,5 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from pet_search.models import Pet
+
+admin.site.register(Pet)
--- pet_search/models.py
+++ pet_search/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Pet(models.Model):
+    SPECIES_CHOICES = (
+        ('dog', 'Dog'),
+        ('cat', 'Cat')
+    )
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    species = models.CharField(max_length=50, choices=SPECIES_CHOICES)
