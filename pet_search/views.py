ab32226cd0ca8d35eebde99e1fbfbdd62e56f9ae
Marc-Andre Boily <mandre.boily@gmail.com>
2016-10-29 20:08:27 -0400

    Added urls.py, templates in pet_search, and added basic homepage view

--- pet_search/views.py
+++ pet_search/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+from pet_search.models import Pet
+
+
+def home(request):
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {
+        'pets': pets
+    })
